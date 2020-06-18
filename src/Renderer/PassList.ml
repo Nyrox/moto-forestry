8eca8fef3059c07762b5d4dfa3d59a66bf173f3a
Nyrox <root@nyrox.tech>
2020-06-18 17:01:28 +0200

    Added main-panel, tab navigation

--- src/Renderer/PassList.ml
+++ src/Renderer/PassList.ml
@@ -9,10 +9,14 @@ type model = {
     createDialog: string option;
 }
 
+type intent =
+    | OpenPass of string
+    | DoNothing
+
 type msg =
     | SetNewFileDialog of string option
     | CreatePass of string
-    | DoNothing
+    | DoubleClickPass of string
 
 
 let init () = 
@@ -23,24 +27,27 @@ let init () =
 
 let update model msg =
     match msg with
-    | SetNewFileDialog msg -> {model with createDialog=msg}
-    | CreatePass name -> 
-        let pass = {name} in
-        { model with createDialog=None; passes=pass::model.passes}
-    | DoNothing -> model
+    | SetNewFileDialog msg -> {model with createDialog=msg}, DoNothing
+    | CreatePass name ->
+        if Option.isSome model.createDialog && String.length name > 0 then
+            let pass = {name} in
+            { model with createDialog=None; passes=pass::model.passes}, OpenPass name
+        else
+            { model with createDialog=None; }, DoNothing
+    | DoubleClickPass p -> model, OpenPass p
 
 open Tea.Html
 
 
-let passItem pass =
-    li [class' "pass-item"] [text pass.name]
+let passItem pass active =
+    li [classList ["pass-item", true; "active", active]; onDoubleClick @@ DoubleClickPass pass.name] [text pass.name]
 
 let viewHeader model = 
     div [class' "header"] 
         [ h2 [] [text "Passes"]
         ; span [class' "button"; onClick @@ SetNewFileDialog (Some "")] @@ [text "+"] ]
 
-let view model: (msg Vdom.t * msg Vdom.t list) =
+let view model (active: string option): (msg Vdom.t * msg Vdom.t list) =
     let createFileDialog = match model.createDialog with
     | None -> Vdom.noNode
     | Some v -> 
@@ -52,7 +59,7 @@ let view model: (msg Vdom.t * msg Vdom.t list) =
                 [ type' "text"
                 ; classList ["pass-item", true; "create-dialog", true]
                 ; id "pass-list-create-dialog-field"
-                ; Tea.Html.onBlur @@ SetNewFileDialog None
+                ; Tea.Html.onBlur @@ CreatePass v
                 ; Tea.Html.value v
                 ; Tea.Html.onInput (fun s -> SetNewFileDialog (Some s))
                 ] []]
@@ -61,7 +68,7 @@ let view model: (msg Vdom.t * msg Vdom.t list) =
         div [Tea.Html.classList [("pass-list", true); ("is-experiencing-input", Option.isSome model.createDialog)]]
             [ viewHeader model
             ; div [class' "pass-items"] @@ List.concat 
-                [ List.map passItem @@ List.rev model.passes
+                [ List.map (fun p -> passItem p (Some p.name == active)) @@ List.rev model.passes
                 ; [ createFileDialog ]
                 ; ]
             ],
--- src/Renderer/Renderer.ml
+++ src/Renderer/Renderer.ml
@@ -6,11 +6,21 @@ open Tea.App
 open Tea.Html
 
 
+type tab =
+    | Input
+    | FragmentShader
+
+type mainWindowState =
+    | Empty
+    | PassWindow of string * tab
+    [@@bs.deriving accessors]
+
 type model = {
     fileTree: FileTree.model;
     global: Global.model;
     editor: MonacoEditor.model;
     passes: PassList.model;
+    mainWindow: mainWindowState;
 }
 
 type msg = 
@@ -18,6 +28,7 @@ type msg =
     | PassListMsg of PassList.msg
     | GlobalMsg of Global.msg
     | EditorMsg of MonacoEditor.msg
+    | TabMainPanelMsg of tab
     | NoOp
     [@@bs.deriving accessors]
 
@@ -28,6 +39,7 @@ let init () =
         global=Global.init ();
         passes=PassList.init ();
         editor=MonacoEditor.init "Ready to roll";
+        mainWindow=Empty
     }, Cmd.map (fun v -> FileTreeMsg v) cmd)
 
 let loadFile: (string -> string) = [%raw {|
@@ -47,14 +59,25 @@ let update (model: model) (msg: msg) =
             {model with fileTree=fileTree}, Cmd.map (fun v -> GlobalMsg v) appCmds;
         ;
     | PassListMsg msg ->
-        let passes = PassList.update model.passes msg in
-        { model with passes=passes }, Cmd.none
+        let passes, intent = PassList.update model.passes msg in
+        match intent with 
+        | OpenPass p ->
+            { model with passes=passes; mainWindow=passWindow p Input }, Cmd.none
+        | DoNothing -> 
+            { model with passes=passes }, Cmd.none;
+        ;
     | GlobalMsg msg -> 
         let globalState = Global.update model.global msg in
         {model with global=globalState}, Cmd.none
     | EditorMsg msg ->
         {model with editor=MonacoEditor.update model.editor msg}, Cmd.none
+    | TabMainPanelMsg tab ->
+        match model.mainWindow with
+        | PassWindow (pass, _) -> { model with mainWindow=passWindow pass tab }, Cmd.none
+        | _ -> raise @@ Failure "Unexpected model state for message";
+        ;
     | NoOp -> model, Cmd.none
+
 
 let sidebar fileTree passList =
     div [class' "sidebar"]
@@ -62,18 +85,69 @@ let sidebar fileTree passList =
         ; Vdom.map (fun v -> FileTreeMsg v) @@ fileTree
         ]
 
+
+let viewVertexDataBlock model pass =
+    div [class' "block"]
+        [ h2 [] [text "Vertex Data"]
+        ]
+
+let viewUniformDataBlock model pass =
+    div [class' "block"]
+        [ h2 [] [text "Uniform Data"]
+        ]
+
+let viewSamplerDataBlock model pass =
+    div [class' "block"]
+        [ h2 [] [text "Samplers"]
+        ]
+
+let viewPassWindow model pass activeTab =
+    let panelTabButton tab =
+        let isActive = tab == activeTab in
+        let label = match tab with
+            | Input -> "Input"
+            | FragmentShader -> "Fragment Shader"
+        in
+        button [onClick @@ tabMainPanelMsg tab; classList [("active", isActive)]] [text label]
+    in
+    let panelNavigation =
+        nav [class' "panel-navigation"]
+        [ panelTabButton Input
+        ; panelTabButton FragmentShader ]
+    in
+    let panelContent = div [class' "panel"] @@ match activeTab with
+        | Input ->
+            [ viewVertexDataBlock model pass
+            ; viewUniformDataBlock model pass
+            ; viewSamplerDataBlock model pass
+            ]
+        | FragmentShader ->
+            [ Vdom.map (fun v -> EditorMsg v) @@ MonacoEditor.view model.editor ]
+    in
+        [panelNavigation; panelContent]
+
+
+let getSelectedPass model =
+    match model.mainWindow with
+    | PassWindow (n, _) -> Some n
+    | _ -> None
+
 let view (model: model)=
     let fileTree, globalItems = FileTree.view model.fileTree in
     let passList, globalItems = 
-        PassList.view model.passes
+        PassList.view model.passes @@ getSelectedPass model
         |> (fun (a, b) -> a, List.concat [List.map (Vdom.map (fun v -> PassListMsg v)) b; List.map (Vdom.map (fun v -> FileTreeMsg v)) globalItems])
     in
     Vdom.node "transparent" [Vdom.style "display" "contents"]
         @@ List.concat [
             [sidebar fileTree passList];
             [div [class' "main-view"]
-                [ Vdom.map (fun v -> EditorMsg v) @@ MonacoEditor.view model.editor;
-                ]];            
+                (match model.mainWindow with
+                | Empty -> []
+                | PassWindow (p, t) -> 
+                    viewPassWindow model p t
+                )
+            ];
             List.map (Vdom.map (fun v -> GlobalMsg v)) @@ Global.view model.global;
             globalItems;
         ]
